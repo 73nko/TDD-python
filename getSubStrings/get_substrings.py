95150797a11e2f1b26c6f24ce584e9837818b051
73nko <apramos89@gmail.com>
2020-01-18 09:26:53 +0100

    TDD kata day3: Find Substrings & return sorted

---
+++ getSubStrings/get_substrings.py
@@ -0,0 +1,8 @@
+def in_array(array1, array2):
+  result = []
+  for word in array1:
+    for possible_substring in array2:
+      if word in possible_substring:
+        result.append(word)
+        break;
+  return sorted(list(dict.fromkeys(result)))
