345dce31684ae9e9706f8f75bc5d97d7fbfdb6f9
73nko <apramos89@gmail.com>
2020-01-19 11:17:03 +0100

    removed semicolon

--- getSubStrings/get_substrings.py
+++ getSubStrings/get_substrings.py
@@ -4,5 +4,5 @@ def in_array(array1, array2):
     for possible_substring in array2:
       if word in possible_substring:
         result.append(word)
-        break;
+        break
   return sorted(list(dict.fromkeys(result)))
