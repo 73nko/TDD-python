fb19d3da19af7707fcb70ef1cd09b20ac6b9b3cf
73nko <apramos89@gmail.com>
2020-01-20 10:32:57 +0100

    Just comment the kata for remember in future

--- SortTheOdd/sort_the_odd.py
+++ SortTheOdd/sort_the_odd.py
@@ -1,3 +1,5 @@
 def sort_array(arr):
   odds = sorted(filter(lambda n: n % 2 != 0, arr))
+  # I had reversed the sorted list at first. 
+  # I've dicoveredt pop 0 and I think is more readable
   return [odds.pop(0) if n%2 != 0 else n for n in arr]
